5590aebb9f22eaa1a1dbc3ad3e5a774c216f0528
Massimiliano Gorreri <max@Mac-mini-di-Massimiliano.local>
2014-03-04 15:38:01 +0100

    Inserimento funzione per le preferenze

---
+++ NocBible/Preferenze.h
@@ -0,0 +1,14 @@
+//
+//  Preferenze.h
+//  NocBible
+//
+//  Created by Massimiliano Gorreri on 04/03/14.
+//  Copyright (c) 2014 Massimiliano Gorreri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MBProgressHUD.h"
+
+@interface Preferenze : UIViewController <MBProgressHUDDelegate>
+
+@end
